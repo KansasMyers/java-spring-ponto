674dc34a31d9c956a0fe63e87883ebd1a5994199
KansasMyers <kansasmyers360@gmail.com>
2020-09-10 11:27:45 -0300

    feat: Commit inicial do projeto

---
+++ src/main/java/br/com/josevictor/api/JavaSpringPontoApplication.java
@@ -0,0 +1,13 @@
+package br.com.josevictor.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringPontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringPontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/josevictor/api/JavaSpringPontoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.josevictor.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringPontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
